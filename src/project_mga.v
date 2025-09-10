8d646269154b72d6c4f36c6cca8d8bb9cf9ae675
markgarnold <78613198+markgarnold@users.noreply.github.com>
2025-09-09 22:18:14 -0400

    Update project_mga.v

--- src/project_mga.v
+++ src/project_mga.v
@@ -8,7 +8,7 @@
 
 `include "spdp7vv.v"
 
-module tt_markgarnold_pdp8 (
+module tt_um_markgarnold_pdp8 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
