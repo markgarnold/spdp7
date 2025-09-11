4b2cff52acb448405ffd8bb7183764a07bc7535c
markgarnold <78613198+markgarnold@users.noreply.github.com>
2025-09-11 13:18:29 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -21,7 +21,7 @@ def init_m():
     m[0o0100] = 0o3510 #7300  # CLA
     m[0o0101] = 0o0442 #1104  # TAD A
     m[0o0102] = 0o1440 #3100 3105  # DCA B
-    m[0o0103] = 0o3201  #7402  # HLT
+    m[0o0103] = 0o3601  #7402  # HLT
     m[0o0104] = 0o1234  # A, data to copy
     m[0o0105] = 0o0000  # B, loc where copy goes
     return m
